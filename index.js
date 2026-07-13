084c7df1bc567e01db6b96b75540338dd7f0a349
John David Garcia <traps-wavy-gristle@duck.com>
2026-07-13 20:10:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -70,14 +70,7 @@ async function sendSpecificMessage() {
         // 2. Wait 1 second for the cursor to actually start blinking
         await new Promise(resolve => setTimeout(resolve, 1000));
 
-        console.log("Uploading debug screenshot...");
-        const screenshotBuffer = await page.screenshot();
-        const response = await fetch('https://transfer.sh/debug.png', {
-            method: 'PUT',
-            body: screenshotBuffer
-        });
-        const screenshotUrl = await response.text();
-        console.log(`👀 LOOK AT THIS URL: ${screenshotUrl}`);
+        await page.screenshot({ path: 'debug.png', fullPage: true });
 
         console.log("Typing message...");
         const lines = targetMessage.split('\n');
